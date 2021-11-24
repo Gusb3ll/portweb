59322f9b9ce8f835e5328cfa4e31dd459b31cc81
Gusb3ll <Kitpipat.0647@gmail.com>
2021-11-24 14:56:45 +0700

    [+] opengraph & reduce tmlcc pic size

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,8 +15,15 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Kitpipat Jaritwong website' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:title', name: 'og:title', content: 'Kitpipat Jaritwong' },
+      { hid: 'og:description ', name: 'og:description' , content: 'Kitpipat Jaritwong website' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:url', name: 'og:url', content: 'https://www.gusbell.tech' },
+      { hid: 'og:image', name: 'og:image', content: 'https://cdn.discordapp.com/attachments/888349851870429184/895346858409476168/og_img.png' },
+      { hid: 'og:image:width', name: 'og:image:width', content: '1200' },
+      { hid: 'og:image:height', name: 'og:image:height', content: '630' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
